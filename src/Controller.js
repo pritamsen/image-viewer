db462cc3b728c021a06d74073685b61c324e73fa
Pritam Sen <pritamsen@gmail.com>
2020-10-24 11:58:21 +0200

    updated Controller file
    updated Controller file

---
+++ src/Controller.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Home from './screens/home/Home';
+import Login from './screens/login/Login';
+import Profile from './screens/Profile/Profile';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class Controller extends Component {
+  
+    render() {
+      return (
+        <Router>
+          <div className="main-container">
+      <Route exact path='/' render={(props)=><Login  {...props}></Login>}/>
+      <Route exact path='/home' render={(props)=><Home  {...props}></Home>} />
+      <Route exact path='/profile' render={(props)=><Profile  {...props}></Profile>} />
+          </div>
+        </Router>
+      )
+    }
+  }
+  
+  export default Controller;
