a668853c2851023056cb964eb0fe487024f23d79
Pritam Sen <pritamsen@gmail.com>
2020-11-02 21:57:03 +0100

    bugfixes controller.js
    bugfixes controller.js

--- src/Controller.js
+++ src/Controller.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import Login from './screens/login/Login';
 import Home from './screens/home/Home';
 import Profile from './screens/profile/Profile';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 class Controller extends Component {
-    render() {
-      return (
+
+  constructor() {
+    super();
+}
+
+render() {
+    return (
         <Router>
-          <div className="main-container">
-      <Route exact path='/' render={(props)=><Login  {...props}></Login>}/>
-      <Route exact path='/home' render={(props)=><Home  {...props}></Home>} />
-      <Route exact path='/profile' render={(props)=><Profile  {...props}></Profile>} />
-          </div>
+            <div className="main-container">
+                <Route exact path='/' render={(props) => <Login {...props}/>}/>
+                <Route path='/home' render={(props) => (sessionStorage.getItem("access-token") !== null ?
+                    <Home {...props} /> :
+                    <Redirect to="/"/>)}/>
+                <Route path='/profile' render={(props) => (sessionStorage.getItem("access-token") !== null ?
+                    <Profile {...props} /> : <Redirect to="/"/>)}/>
+            </div>
         </Router>
-      )
-    }
-  }
+    )
+}
+}
   export default Controller;
